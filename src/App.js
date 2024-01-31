4dca0d34680147dac96f97b4e94979e335ad5426
Alexandre Poirier <AlexPoirier1@users.noreply.github.com>
2024-01-31 03:09:20 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Alexandre Poirier</h1>
+                <h3>Senior Software Engineer</h3>
+            </header>
+        </div>
+    )
+}
+
+export default App
